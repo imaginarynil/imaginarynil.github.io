4985ea86d72e0fe0ac465ae561e1dca18c433c4e
imaginarynil <sugianto.daniel@proton.me>
2024-06-18 12:58:06 -0400

    refactor app bar

--- component/appBar.js
+++ component/appBar.js
@@ -1,14 +1,14 @@
-function createNavLinkListEl(links, listClassName, itemClassName) {
-    const navLinksEl = document.createElement("div")
-    navLinksEl.classList.add(listClassName)
+function createNavigationEl(links, listClassName, itemClassName) {
+    const navigationEl = document.createElement("nav")
+    navigationEl.classList.add(listClassName)
     for (let i = 0; i < links.length; i++) {
-        const navLinkEl = document.createElement("a")
-        navLinkEl.classList.add(itemClassName)
-        navLinkEl.appendChild(document.createTextNode(links[i].name))
-        navLinkEl.href = links[i].url
-        navLinksEl.appendChild(navLinkEl)
+        const navigation__linkEl = document.createElement("a")
+        navigationEl.appendChild(navigation__linkEl)
+        navigation__linkEl.classList.add(itemClassName)
+        navigation__linkEl.appendChild(document.createTextNode(links[i].name))
+        navigation__linkEl.href = links[i].url
     }
-    return navLinksEl
+    return navigationEl
 }
 
 function addAppBar() {
@@ -19,45 +19,36 @@ function addAppBar() {
         { name: "About", url: "/about/index.html" }
     ]
     const appBarEl = document.querySelector(".app-bar")
-    const navContainerEl = document.createElement("div")
-    appBarEl.appendChild(navContainerEl)
-    navContainerEl.classList.add("nav-container")
-    const navLogoEl = document.createElement("a")
-    navContainerEl.appendChild(navLogoEl)
-    navLogoEl.classList.add("nav-logo")
-    navLogoEl.href = "/index.html"
-    navLogoEl.appendChild(document.createTextNode("imaginarynil"))
-    const navLinksContainerEl = document.createElement("div")
-    navContainerEl.appendChild(navLinksContainerEl)
-    navLinksContainerEl.classList.add("nav-links-container")
-    navLinksContainerEl.appendChild(
-        createNavLinkListEl(
-            navLinks,
-            "nav-links",
-            "nav-link"
-        )
-    )
-    const navToggleBtnEl = document.createElement("a")
-    navContainerEl.appendChild(navToggleBtnEl)
-    navToggleBtnEl.classList.add("nav-toggle-btn")
-    navToggleBtnEl.href = "javascript:void(0);"
-    // button icon
-    navToggleBtnEl.innerHTML = `
-            <svg class="nav-toggle-btn-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+    const appBar_innerEl = document.createElement("div")
+    appBarEl.appendChild(appBar_innerEl)
+    appBar_innerEl.classList.add("app-bar__inner")
+    const logoEl = document.createElement("a")
+    appBar_innerEl.appendChild(logoEl)
+    logoEl.classList.add("logo", "app-bar__logo")
+    logoEl.href = "/index.html"
+    logoEl.appendChild(document.createTextNode("imaginarynil"))
+    appBar_innerEl.appendChild(createNavigationEl(
+        navLinks,
+        "navigation",
+        "navigation__link"
+    ))
+    const appBar_togglerEl = document.createElement("a")
+    appBar_innerEl.appendChild(appBar_togglerEl)
+    appBar_togglerEl.classList.add("app-bar__toggler")
+    appBar_togglerEl.href = "javascript:void(0);"
+    appBar_togglerEl.innerHTML = `
+            <svg class="app-bar__toggler-icon" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
                 <path
                     d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
             </svg>
             `
-    navToggleBtnEl.onclick = function () {
-        let navDropdownEl = document.querySelector(".nav-dropdown");
-        navDropdownEl.classList.toggle("nav-dropdown-opened")
+    appBar_togglerEl.onclick = function () {
+        let mobileNavigationEl = document.querySelector(".mobile-navigation");
+        mobileNavigationEl.classList.toggle("mobile-navigation_opened")
     }
-    const navDropdownEl = document.createElement("div")
-    appBarEl.appendChild(navDropdownEl)
-    navDropdownEl.classList.add("nav-dropdown")
-    navDropdownEl.appendChild(createNavLinkListEl(
+    appBarEl.appendChild(createNavigationEl(
         navLinks,
-        "nav-dropdown-links",
-        "nav-dropdown-link"
+        "mobile-navigation",
+        "mobile-navigation__link"
     ))
 }
