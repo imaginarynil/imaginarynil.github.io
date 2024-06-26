7414c50544f9098fef9d0ef4576ee84a16f5b40f
imaginarynil <sugianto.daniel@proton.me>
2024-06-25 21:46:29 -0400

    add tag-list to post-item, refactor post-list, and fix sidebar

--- block/tagList.js
+++ block/tagList.js
@@ -1,23 +1,30 @@
+function appendItemsToList(tagListEl, postId, postTagTable, tagTable) {
+    for (const [_, postTagRow] of Object.entries(postTagTable)) {
+        if (postTagRow.postId === postId) {
+            const tagListItemEl = document.createElement("li")
+            tagListEl.appendChild(tagListItemEl)
+            tagListItemEl.classList.add("tag-list__item")
+            tagListItemEl.appendChild(document.createTextNode(tagTable[postTagRow.tagId].name))
+        }
+    }
+}
+
+function createTagListEl(postId, postTagTable, tagTable) {
+    const tagListEl = document.createElement("ul")
+    tagListEl.classList.add("tag-list")
+    appendItemsToList(tagListEl, postId, postTagTable, tagTable)
+    return tagListEl
+}
+
 function addTagList(postId) {
     const tagListEl = document.querySelector(".tag-list")
-    fetch("/data/post.json")
+    fetch("/data/post_tag.json")
         .then(res => res.json())
-        .then(postTable => {
-            fetch("/data/post_tag.json")
+        .then(postTagTable => {
+            fetch("/data/tag.json")
                 .then(res => res.json())
-                .then(postTagTable => {
-                    fetch("/data/tag.json")
-                        .then(res => res.json())
-                        .then(tagTable => {
-                            for (const [_, val] of Object.entries(postTagTable)) {
-                                if (val.postId === postId) {
-                                    const tagListItemEl = document.createElement("li")
-                                    tagListEl.appendChild(tagListItemEl)
-                                    tagListItemEl.classList.add("tag-list__item")
-                                    tagListItemEl.appendChild(document.createTextNode(tagTable[val.tagId].name))
-                                }
-                            }
-                        })
+                .then(tagTable => {
+                    appendItemsToList(tagListEl, postId, postTagTable, tagTable)
                 })
         })
 }
