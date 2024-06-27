2bae4bf2576b1906f327293406f28d0a6ed716e9
imaginarynil <sugianto.daniel@proton.me>
2024-06-27 14:31:46 -0400

    modularize footer

---
+++ block/footer.js
@@ -0,0 +1,50 @@
+function createAccountListEl() {
+    const footer__accountListEl = document.createElement("ul")
+    footer__accountListEl.classList.add("footer__account-list")
+    const accounts = [
+        {
+            name: "Github",
+            url: "https://github.com/imaginarynil"
+        }
+    ]
+    for (let i = 0; i < accounts.length; i++) {
+        const footer__accountItemEl = document.createElement("li")
+        footer__accountListEl.appendChild(footer__accountItemEl)
+        footer__accountItemEl.classList.add("footer__account-item")
+        const footer__accountLinkEl = document.createElement("a")
+        footer__accountItemEl.appendChild(footer__accountLinkEl)
+        footer__accountLinkEl.classList.add("footer__account-link")
+        footer__accountLinkEl.href = accounts[i].url
+        footer__accountLinkEl.appendChild(
+            document.createTextNode(accounts[i].name)
+        )
+    }
+    return footer__accountListEl
+}
+
+function addFooter() {
+    const footerEl = document.querySelector(".footer")
+    const footer__layoutEl = document.createElement("div")
+    footerEl.appendChild(footer__layoutEl)
+    footer__layoutEl.classList.add("footer__layout")
+    const footer__accountListEl = createAccountListEl()
+    footer__layoutEl.appendChild(footer__accountListEl)
+    const footer__messageEl = document.createElement("div")
+    footer__layoutEl.appendChild(footer__messageEl)
+    footer__messageEl.classList.add("footer__message")
+    footer__messageEl.appendChild(
+        document.createTextNode("Built using vanilla HTML, CSS, and JS")
+    )
+    const footer__copyrightEl = document.createElement("div")
+    footer__layoutEl.appendChild(footer__copyrightEl)
+    footer__copyrightEl.classList.add("footer__copyright")
+    footer__copyrightEl.appendChild(
+        document.createTextNode("Created by Daniel Sugianto ")
+    )
+    const footer__yearEl = document.createElement("span")
+    footer__copyrightEl.appendChild(
+        footer__yearEl
+    )
+    footer__yearEl.classList.add("footer__year")
+    footer__yearEl.innerHTML = `(${new Date().getFullYear()})`;
+}
